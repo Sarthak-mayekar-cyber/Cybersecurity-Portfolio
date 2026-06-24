0506468d2778fe68ae719003856a62c7af0d1473
Sarthak Sandeep Mayekar <sarthakmayekar09@gmail.com>
2026-06-24 22:47:29 +0530

    Implement password strength evaluator
    Added a password strength evaluator that checks length, uppercase letters, digits, and special characters.

--- Projects Cybersecurity-Toolkit password_checker.py
+++ Projects Cybersecurity-Toolkit password_checker.py
@@ -1,1 +1,25 @@
 # simple password strength evaluator
+password = input("Enter your password")
+
+score = 0
+
+if len(password) >= 8:
+    score += 1
+
+if any(c.isupper() for c in password):
+    score += 1
+
+if any(c.isdigit() for c in password):
+    score += 1
+
+if any(not c.isalnum() for c in password ):
+    score += 1
+
+if score <=1:
+    print("Weak Password")
+elif score <= 3:
+    print("Medium password")
+else:
+    print("Strong password")
+    
+
